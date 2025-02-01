996ac151199c1d8a71dfba5b2e3c845c2dae2972
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2025-02-01 22:29:42 +0530

    added create tasks button

--- src/pages/AllTasks.jsx
+++ src/pages/AllTasks.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import Cards from '../components/Cards'
+import { MdAddCircleOutline } from 'react-icons/md'
 
 function AllTasks() {
     return (
         <div>
+            <div className='w-full flex justify-end px-4 py-2'>
+                <button>
+                    <MdAddCircleOutline className='text-5xl text-gray-500 hover:text-gray-100 cursor-pointer transition-all duration-300' />
+
+                </button>
+            </div>
+
             <Cards />
         </div>
     )
