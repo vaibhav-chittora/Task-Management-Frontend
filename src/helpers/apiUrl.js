294dea80d729c3e67edd3b1bdf4c681e3753c5aa
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2025-02-06 23:49:19 +0530

    configured env file

--- src/helpers/apiUrl.js
+++ src/helpers/apiUrl.js
@@ -1,1 +1,7 @@
-export const backendApiUrl = "http://localhost:3000/api/v1";
+// export const backendApiUrl = "http://localhost:3000/api/v1";
+
+export const backendApiUrl = import.meta.env.VITE_API_backendApiUrl;
+
+console.log("Backend API URL - ", backendApiUrl);
+
+export const BACKEND_API_URL = import.meta.env.VITE_BACKEND_API_URL;
