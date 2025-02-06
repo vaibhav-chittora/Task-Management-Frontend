56b0188c2813ae09d3def75a09b1be2aaf66b18a
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2025-02-06 22:21:45 +0530

    implemented task creation

--- src/components/InputModal.jsx
+++ src/components/InputModal.jsx
@@ -1,9 +1,59 @@
 import React, { useState } from 'react'
 import { MdOutlineClose } from 'react-icons/md'
+import axiosInstance from '../helpers/axiosInstance'
 
 function InputModal({ showModal, setShowModal }) {
 
     // const [showModal, setShowModal] = useState('hidden')
+    const [data, setData] = useState({
+        title: '',
+        description: '',
+    })
+
+    const userDetails = {
+        username: localStorage.getItem('user'),
+        email: localStorage.getItem('email'),
+        // token: localStorage.getItem('token')
+        authorization: localStorage.getItem('token')
+    }
+
+    const onChangeHandler = (e) => {
+        const { name, value } = e.target
+        setData({ ...data, [name]: value })
+    }
+
+    const createTaskHandler = async () => {
+        const { title, description } = data
+        try {
+            if (!title || !description) {
+                alert('Please fill all the fields')
+                return
+            }
+            const response = await axiosInstance.post('/task/create-task',
+                {
+                    title,
+                    description
+                },
+                {
+                    headers: userDetails
+                }
+            )
+            console.log("task creating Response - ", response);
+            setData({
+                title: '',
+                description: '',
+            })
+            setShowModal('hidden')
+            windowdow.location.reload()
+
+        } catch (error) {
+            console.log("Error - ", error);
+            alert('Something went wrong while creating task')
+
+
+        }
+    }
+
     return (
         <>
             <div className={`${showModal} top-0 left-0 bg-gray-600 opacity-50 h-screen w-full`}></div>
@@ -27,16 +77,25 @@ function InputModal({ showModal, setShowModal }) {
                         type="text"
                         placeholder='Title'
                         name='title'
+                        value={data.title}
+                        onChange={onChangeHandler}
                     />
                     <textarea
                         className='w-full bg-slate-700 p-2 rounded-md my-3'
                         placeholder='Enter your tasks description here.'
                         rows="4"
                         cols="50"
+                        name='description'
+                        value={data.description}
+                        onChange={onChangeHandler}
                     >
 
                     </textarea>
-                    <button className='bg-blue-500 text-2xl px-3 py-2 rounded-md my-3 '>
+                    <button
+                        className='bg-blue-500 text-2xl px-3 py-2 rounded-md my-3 '
+                        onClick={createTaskHandler}
+                    >
+
                         Create
                     </button>
                 </div>
